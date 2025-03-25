857333a954f6a97cf35fe2a2cbb88dcc19d9cc26
Iyiou <undertlx@outlook.com>
2025-03-24 23:14:57 -0300

    feat: jump mechanics

--- Assets/Scripts/Player/PlayerLogic.cs
+++ Assets/Scripts/Player/PlayerLogic.cs
@@ -12,7 +12,16 @@ public class PlayerLogic : MonoBehaviour
     public LimitPlayer leftCollision;
     public LimitPlayer footCollision;
     public float speed;
+    public float jumpY; // Variável que define a força de pulo
+    private bool isJumping; // Variável de estado de pulo
+    
+    public bool doubleJump;
+    private Coroutine coroutineJump; // Variável para limitar o tempo de pulo 
 
+    public Rigidbody2D rigidbody2d; // Variável para acessar propriedades físicas do player;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +33,7 @@ public class PlayerLogic : MonoBehaviour
     {
         // Move
         Move();
+        Jump();
 
     }
     //Movimentação do player
@@ -49,4 +59,63 @@ public class PlayerLogic : MonoBehaviour
         transform.position += direction * speed * Time.deltaTime;
 
     }
+
+    private void Jump()
+    {
+        // Input jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (footCollision.isLimit == true && isJumping == false)
+            {
+                isJumping = true;
+
+
+
+                ActivateJumpTime();
+            }
+
+
+        }
+        // Pular
+        Jumping();
+
+    }
+    
+    // Reset do pulo
+    private void ActivateJumpTime()
+    {
+        if (coroutineJump != null)
+        {
+            StopCoroutine(coroutineJump);
+        }
+        coroutineJump = StartCoroutine(JumpTime());
+    }
+    
+    // Contador para tempo de pulo
+    private IEnumerator JumpTime()
+    {
+        yield return new WaitForSeconds(0.3f); // Permitir 0.3s para o player pular
+        isJumping = false;// desativa a variável de pulo
+    }
+
+
+    private void Jumping()
+    {
+        if (isJumping == true)
+        {
+            if (headCollision.isLimit == false)
+            { // Verifica se a há colisão acima do player
+                GetComponent<Rigidbody2D>().velocity = Vector3.zero; // |erar forças nos eixos do rigidbody2D                
+                GetComponent<Rigidbody2D>().gravityScale = 0; // Altera a propriedade para fazer o player subir
+                Vector3 jumpDirection = new Vector3(0, jumpY, 0); // Direcionar o pulo
+                transform.position += jumpDirection * speed * Time.deltaTime; // Pulo
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 4; // Faz o player cair
+            }
+
+        }
+    }
+
 }
