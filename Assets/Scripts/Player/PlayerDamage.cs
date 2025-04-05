608dc425f86ceb78f47c029376ca77174300995e
Iyiou <undertlx@outlook.com>
2025-04-05 14:50:23 -0300

    feat: player damage animation

--- Assets/Scripts/Player/PlayerDamage.cs
+++ Assets/Scripts/Player/PlayerDamage.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerDamage : MonoBehaviour
 {
     public PlayerLogic playerLogic; // Variável para acessar metodos da lógica do jogador
     
+    public AnimationPlayer animationPlayer;
+    
     //Efetuar dano ao jogador
     public void Damage(){
         
+        animationPlayer.Damage(); // Ativar animação de dano
         playerLogic.ResetPhysicalMove();    // Resetar forças
         
         playerLogic.ThrowPlayer();  // Arremessar
