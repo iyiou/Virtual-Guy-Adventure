ec19032401e8893eaed56d470711cfe34d1a557a
Iyiou <undertlx@outlook.com>
2025-04-25 14:12:25 -0300

    feat: creating player controller, setting properties and references

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerLogic playerLogic;
+    private AnimationPlayer animationPlayer;
+    private PlayerDamage playerDamage;
+
+
+    // Propriedades de acesso
+    public PlayerLogic PlayerLogic => playerLogic;
+    public Animation AnimationPlayer => animationPlayer;
+    public PlayerDamage PlayerDamage => playerDamage;
+   
+
+    void Awake()
+    {
+        //Obeter referência
+        playerLogic = GetComponent<PlayerLogic>(); // Jogador 
+        animationPlayer = GetComponentInChildren<AnimationPlayer>(); // Animações
+        playerDamage = GetComponent<PlayerDamage>(); // Dano ao Jogador
+    }
+
+}
